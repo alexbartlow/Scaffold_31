e54547cdfff86cf43515db4c167c6e72faaa2940
alex <alex@alex-VirtualBox.(none)>
2011-10-30 23:18:26 -0400

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scaffold31::Application.config.secret_token = '36378a1f61f50e8634438396b2aabee6289c8336111d897cdb9e05127009cf4083554a471850dc94f3fb6b352d152e32dbe068c749dd295fbb564f0e0d2c3f8e'
